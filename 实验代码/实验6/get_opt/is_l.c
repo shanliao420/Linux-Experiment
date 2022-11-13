dc8ebf48882105b31a89e363a76fefac3b640482
shanliao <shanliao420@yeah.net>
2022-11-13 21:32:17 +0800

    get_opt 11.13

--- 实验代码/实验6/get_opt/is_l.c
+++ 实验代码/实验6/get_opt/is_l.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 #include "is_l.h"
+#include "get_opt_size.h"
 
 // 判断是否有-l 参数 有返回1 无返回0
-int is_l(char const *argv[]){
-    return 1;
+int is_l(int argc, char const *argv[]){
+    switch (get_opt_size(argc))
+    {
+    case 1:
+        if(!strcmp("-l", argv[1])){
+            return 1;
+        }
+        break;
+    case 2:
+        if(!strcmp("-l", argv[2])){
+            return 1;
+        }
+        break;
+    default:
+        return 0;
+    }
+    return 0;
 }
 
 
 
 int main(int argc, char const *argv[])
 {
-    if(is_l(argv)){
+    if(is_l(argc, argv)){
         printf("have -l parameter\n");
     }else{
         printf("no -l parameter\n");
