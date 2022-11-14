c4c7fbf70e9b89f08ed414f59e34a48712291a68
shanliao <shanliao420@yeah.net>
2022-11-14 20:22:18 +0800

    shiting add show file names 11.14

--- 实验代码/实验6/print_file/show_file_names.c
+++ 实验代码/实验6/print_file/show_file_names.c
@@ -1,9 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<dirent.h>
+#include<stdlib.h>
 #include "show_file_names.h"
 
 
-void show_file_names(char* path){
-    printf("printf all of the dir's file-names\n");
+void show_file_names(char dirname[])
+{
+    DIR *dir_ptr;//文件夹指针
+    struct dirent *direntp;//存放文件夹目录内容
+
+    if((dir_ptr = opendir(dirname)) == NULL){//open环节
+        fprintf(stderr, "ls1: open error %s\n",dirname);//打开失败
+    }else{
+        while((direntp = readdir(dir_ptr)) != NULL){//while（readdir）
+            printf("%s\n",direntp -> d_name);//d_name:当前遍历子项的文件名，限制长度为256，包括字符串终止符’\0’
+        }
+        int flag;
+        if(( flag = closedir(dir_ptr)) == -1){//close环节
+            perror("ls1: close dir error");//关闭失败
+            exit(0);
+        }
+ }
 }
 
 
