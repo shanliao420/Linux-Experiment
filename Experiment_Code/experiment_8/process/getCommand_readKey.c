ae92ec1b88a5414a2646a186669066699b886404
喜欢吃干脆面的糖 <shanliao420@yeah.net>
2022-11-24 17:08:29 +0800

    update readkey 11.24

--- Experiment_Code/experiment_8/process/getCommand_readKey.c
+++ Experiment_Code/experiment_8/process/getCommand_readKey.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "getCommand_readKey.h"
+#define MAXSIZE 4096
 
 
 void readKey(char* buff){
-    printf("读取键盘输入一行到buff\n");
+    char* ret;
+    ret = fgets(buff, MAXSIZE, stdin);
+    if(ret == NULL){
+        printf("error occurs!\n");
+        exit(1);
+    }
 }
+
+
+// int main(int argc, char const *argv[])
+// {
+    
+//     char buff[MAXSIZE];
+//     readKey(buff);
+//     printf("the buff is %s\n", buff);
+
+//     return 0;
+// }
